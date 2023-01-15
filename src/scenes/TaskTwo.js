c9e73ef5a9ea08860e27dabce5ec371ac11144d9
Ankie Pon <anpon@ucsc.edu>
2023-01-15 15:26:38 -0800

    mixing mechanic

--- src/scenes/TaskTwo.js
+++ src/scenes/TaskTwo.js
@@ -4,19 +4,30 @@ class TaskTwo extends Phaser.Scene {
     }
     preload(){
       this.load.image('batter', './assets/batter.png');
+      this.load.image('mixing_bowl', './assets/mixing_bowl.png');
     }
 
     create() {
       //let current_scene = this;
       this.add.text(20, 20, "Press space to mix the batter! Esc to go back!");
-
-      let batter = this.add.sprite(100, 200,'batter').setOrigin(.5, .5).setInteractive();
+      this.mixing_bowl = this.add.sprite(540, 350,'mixing_bowl').setOrigin(.5, .5).setInteractive();
+      this.batter = this.add.sprite(100, 200,'batter').setOrigin(.5, .5).setInteractive();
+      this.batter2 = this.add.sprite(150, 200,'batter').setOrigin(.5, .5).setInteractive();
+      this.batter3 = this.add.sprite(100, 250,'batter').setOrigin(.5, .5).setInteractive();
       
       let yoinked = null;
-      
+  
       //batter
-      batter.on('pointerdown', function() {
-        yoinked = batter;
+      this.batter.on('pointerdown', function() {
+        yoinked = this.batter;
+      }, this);
+      //batter2
+      this.batter2.on('pointerdown', function() {
+        yoinked = this.batter2;
+      }, this);
+      //batter3
+      this.batter3.on('pointerdown', function() {
+        yoinked = this.batter3;
       }, this);
 
       this.input.on('pointerup', function() {
@@ -36,12 +47,27 @@ class TaskTwo extends Phaser.Scene {
     }
 
     update() {
+      //collision checking
+      if(this.collisioncheck(this.batter, this.mixing_bowl) || this.collisioncheck(this.batter2, this.mixing_bowl) 
+            || this.collisioncheck(this.batter3, this.mixing_bowl)) {
+        console.log('ingredient in bowl');
+      }
+
       if(Phaser.Input.Keyboard.JustDown(keySPACE) && ordered){
+        console.log('mixed');
         mixed=true;
        }
 
       if(Phaser.Input.Keyboard.JustDown(keyESC)){
         this.scene.start('SceneOne');
        }
+    }
+
+    collisioncheck(ingredient, in_bowl) {
+      if (ingredient.x+50 >= in_bowl.x && ingredient.x-50 <= in_bowl.x + in_bowl.width*.75 && 
+        ingredient.y+50 >= in_bowl.y && ingredient.y-50 <= in_bowl.y + in_bowl.height*.75) {
+        ingredient.alpha = 0;
+      }
+
     }
 }
